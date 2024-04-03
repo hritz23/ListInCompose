404483c60b0435876a8a1bc80986b16e63b4de54
HritikaNaik <hritikan232@gmail.com>
2024-04-03 16:00:07 +0530

    find puppy data by id

--- app/src/main/java/com/example/listincompose/ProfileDetail.kt
+++ app/src/main/java/com/example/listincompose/ProfileDetail.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -19,6 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.listincompose.data.DataProvider
 import com.example.listincompose.ui.theme.ListInComposeTheme
 
 class ProfileDetail : ComponentActivity() {
@@ -31,26 +33,28 @@ class ProfileDetail : ComponentActivity() {
 }
 
 @Composable
-fun ProfileNavigation(navController: NavController, puppyId: String) {
+fun ProfileNavigation(navController: NavController, puppyId: Int) {
     ProfileDetails(puppyId)
 }
 
 @Composable
-fun ProfileDetails(puppyId: String, modifier: Modifier = Modifier.fillMaxSize()) {
+fun ProfileDetails(puppyId: Int, modifier: Modifier = Modifier.fillMaxSize()) {
+    val puppies = remember { DataProvider.puppyList } // Remember the puppy list
+    val puppy = puppies.find { it.id == puppyId }
     Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         Image(
-            painter = painterResource(R.drawable.p1),
+            painter = painterResource(puppy!!.imageId),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = modifier.weight(0.5f)
         )
         Text(
-            text = puppyId,
+            text = puppy.title,
             modifier.padding(16.dp),
             style = MaterialTheme.typography.headlineSmall
         )
-        Text(text = "Monty", modifier.padding(16.dp), fontSize = 12.sp)
-        Text(text = "Monty", modifier.padding(16.dp), fontSize = 12.sp)
+        Text(text = puppy.gender, modifier.padding(16.dp), fontSize = 12.sp)
+        Text(text = puppy.description, modifier.padding(16.dp), fontSize = 12.sp)
     }
 }
 
@@ -58,6 +62,6 @@ fun ProfileDetails(puppyId: String, modifier: Modifier = Modifier.fillMaxSize())
 @Composable
 fun GreetingPreview2() {
     ListInComposeTheme {
-        ProfileDetails(puppyId = "5")
+        ProfileDetails(2)
     }
 }
