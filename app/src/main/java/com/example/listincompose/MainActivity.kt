3b88b5aff5bbb59ab985441f2dfdc427590dabbd
HritikaNaik <hritikan232@gmail.com>
2024-04-03 17:03:47 +0530

    added elevation, color, border to card

--- app/src/main/java/com/example/listincompose/MainActivity.kt
+++ app/src/main/java/com/example/listincompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -127,7 +129,10 @@ fun PuppyListItem(puppy: Puppy, onItemClick: (Puppy) -> Unit) {
             .padding(8.dp)
             .fillMaxWidth()
             .clickable { onItemClick(puppy) }, // Adding clickable modifier here
-        elevation = CardDefaults.elevatedCardElevation()
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+        border = BorderStroke(1.dp, Color.DarkGray)
+
     ) {
         Row() {
             Image(
