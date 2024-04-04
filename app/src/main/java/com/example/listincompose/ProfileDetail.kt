569da4b9f7d329325d48c3b1c79ffcc5c840b8c1
HritikaNaik <hritikan232@gmail.com>
2024-04-04 16:32:41 +0530

    add animatedVisibility clickableText

--- app/src/main/java/com/example/listincompose/ProfileDetail.kt
+++ app/src/main/java/com/example/listincompose/ProfileDetail.kt
@@ -3,6 +3,7 @@ package com.example.listincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
@@ -14,17 +15,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -50,6 +56,7 @@ fun ProfileNavigation(navController: NavController, puppyId: Int) {
 fun ProfileDetails(puppyId: Int, modifier: Modifier = Modifier.fillMaxSize()) {
     val puppies = remember { DataProvider.puppyList } // Remember the puppy list
     val puppy = puppies.find { it.id == puppyId }
+    var showDetails by remember { mutableStateOf(false) }
     Surface {
         Column(
             modifier = Modifier
@@ -62,7 +69,18 @@ fun ProfileDetails(puppyId: Int, modifier: Modifier = Modifier.fillMaxSize()) {
                 puppy!!,
                 modifier.fillMaxHeight(1f)
             )
-            ProfileContent(puppy, modifier.fillMaxHeight(1f))
+            ClickableText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall,
+                text = AnnotatedString("View All"),
+                onClick = { showDetails = !showDetails })
+            AnimatedVisibility(showDetails) {
+                ProfileContent(puppy, modifier.fillMaxHeight(1f))
+            }
+
+
         }
     }
 }
