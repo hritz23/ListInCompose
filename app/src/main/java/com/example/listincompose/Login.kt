d7c0ddca749d1a508aebc71499199da4ed2bc59c
HritikaNaik <hritikan232@gmail.com>
2024-04-05 16:01:13 +0530

    added elevated button, topAppbar, bottombar, floatingButton

--- app/src/main/java/com/example/listincompose/Login.kt
+++ app/src/main/java/com/example/listincompose/Login.kt
@@ -3,42 +3,127 @@ package com.example.listincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.twotone.Info
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.listincompose.ui.theme.ListInComposeTheme
 
 class Login : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ListInComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
 
-                }
+
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarApp() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = { Text(text = "Login") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "BackArrow"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "check"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.TwoTone.Info,
+                            contentDescription = "info"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.Person,
+                            contentDescription = "person"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "fav"
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(Icons.Default.Add, "Add")
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        MyLoginApp(modifier = Modifier.padding(innerPadding))
     }
 }
 
@@ -46,36 +131,73 @@ class Login : ComponentActivity() {
 fun OutLineTextFieldSample() {
     var name by remember { mutableStateOf(TextFieldValue("")) }
     var email by remember { mutableStateOf(TextFieldValue("")) }
-    Column(modifier = Modifier.padding(16.dp)) {
+    var pwd by remember { mutableStateOf(TextFieldValue("")) }
+    Column(modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         OutlinedTextField(
             value = name,
             label = { Text(text = "Full Name") },
             onValueChange = {
                 name = it
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(30),
         )
         OutlinedTextField(
-            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription ="Email Icon")},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email Icon"
+                )
+            },
 //            trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription ="Add")},
             value = email,
             label = { Text(text = "Email") },
             onValueChange = { email = it },
             modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = "Enter your mail") }
+            placeholder = { Text(text = "Enter your mail") },
+            shape = RoundedCornerShape(30)
+        )
+        OutlinedTextField(
+            value = pwd,
+            label = { Text(text = "Password") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            onValueChange = {
+                pwd = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(30)
+//            visualTransformation = PasswordVisualTransformation()
         )
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MyLoginApp() {
-    OutLineTextFieldSample()
+fun Buttons() {
+    ElevatedButton(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier.padding(16.dp)
+
+    ) {
+        Icon(imageVector = Icons.Default.Check, contentDescription = "Submit")
+        Text(text = "Submit", modifier = Modifier.padding(start = 8.dp))
+    }
+}
+
+@Composable
+fun MyLoginApp(modifier: Modifier) {
+    Column(modifier = modifier) {
+        OutLineTextFieldSample()
+        Buttons()
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun LoginPreview() {
     ListInComposeTheme {
-        MyLoginApp()
+        TopBarApp()
     }
 }
